5b31eb2bd84001b57b9917e0c5e8abf11793c8a4
Hitten-Chaudhary11 <harshitc956@gmail.com>
2026-07-07 13:25:36 +0530

    Time: 21 ms (8.21%) | Memory: 42.5 MB (61.06%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+     //   return (int)Math.sqrt(x);
+
+      int i = 0;
+
+        while ((long) i * i <= x) {
+            i++;
+        }
+
+        return i - 1;
     }
 }
